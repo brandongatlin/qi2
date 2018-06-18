a4d8b7fb13685e114082b7efdac68c359e909472
brandongatlin <brandongatlin1981@me.com>
2018-06-18 17:16:57 -0500

    fixed sticky footer

--- sticky.js
+++ sticky.js
@@ -1,22 +1,30 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+  myFunction()
+};
 
 // Get the header
 var header = document.getElementById("myHeader");
+var footer = document.getElementById("myFooter");
+
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var stickyHeader = header.offsetTop;
+var stickyFooter = footer.offsetTop;
+
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  console.log('scrolling');
-  if (window.pageYOffset >= sticky) {
-    console.log('sticky');
-
+  if (window.pageYOffset >= stickyHeader) {
     header.classList.add("sticky");
-  } else {
-    console.log('not!');
+    footer.classList.remove("sticky");
+  }
 
+  if (window.pageYOffset >= stickyFooter) {
+    footer.classList.add("sticky");
     header.classList.remove("sticky");
   }
+
+
+
 }
